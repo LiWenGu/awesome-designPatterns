9c7c7d1e77979d73d2bcdeef4e65850cc42a8c55
liwenguang_dev@baletoo.com <liwenguang_dev@baletoo.com>
2018-11-02 18:04:36 +0800

    proxy pattern

---
+++ src/main/java/proxy/ext_dynamic/IGamePlayer.java
@@ -0,0 +1,12 @@
+package proxy.ext_dynamic;
+
+public interface IGamePlayer {
+    // 登录游戏
+    public void login(String user, String password);
+
+    // 杀怪，网络游戏的主要特色
+    public void killBoss();
+
+    // 升级
+    public void upgrade();
+}
