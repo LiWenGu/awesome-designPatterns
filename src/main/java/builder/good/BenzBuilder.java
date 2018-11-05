19610d5d7fa4d68aaf2b452a30f1de12ca3ff375
liwenguang_dev@baletoo.com <liwenguang_dev@baletoo.com>
2018-11-05 11:58:51 +0800

    builder pattern

---
+++ src/main/java/builder/good/BenzBuilder.java
@@ -0,0 +1,18 @@
+package builder.good;
+
+
+import java.util.ArrayList;
+
+public class BenzBuilder extends CarBuilder {
+    private BenzModel benz = new BenzModel();
+
+    @Override
+    public CarModel getCarModel() {
+        return this.benz;
+    }
+
+    @Override
+    public void setSequence(ArrayList<String> sequence) {
+        this.benz.setSequence(sequence);
+    }
+}
---
+++ src/main/java/builder/standard/Product.java
@@ -0,0 +1,7 @@
+package builder.standard;
+
+public class Product {
+    public void doSomething() {
+        //独立业务处理
+    }
+}
